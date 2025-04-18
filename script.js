a36b8d0707b276e8ea7510ee03dc5cf843246b0e
aurenaa <salapuranev@gmail.com>
2025-04-18 17:17:18 +0200

    hover effect on 16x16 grid

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ function createInitialGrid(){
     for (var i = 0; i < 256; i++){
         const divBox = document.createElement("div");
         divBox.classList.add("divBox");
+
+        divBox.addEventListener('mouseover', function() {
+            divBox.style.backgroundColor = "grey";
+        });
+
         containerEl.appendChild(divBox);
     }
 }
